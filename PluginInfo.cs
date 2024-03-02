2693a845992a1db0a250ec323b0b9df3d49b7ffa
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2024-03-02 17:38:22 -0500

    Delete PluginInfo.cs

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LCSymphony
-{
-    public class PluginInfo
-    {
-        public const string PLUGIN_GUID = "dev.alexanderdiaz.lcsymphony";
-        public const string PLUGIN_NAME = "LC Symphony";
-        public const string PLUGIN_VERSION = "1.3.0";
-    }
-}
